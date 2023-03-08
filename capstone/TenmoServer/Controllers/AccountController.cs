c2aabd715d0c6772691f1857a72c0428321dc680
Tahni Hresko <tahni.jae@gmail.com>
2023-03-08 11:23:35 -0500

    Making program skeleton

---
+++ capstone/TenmoServer/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
