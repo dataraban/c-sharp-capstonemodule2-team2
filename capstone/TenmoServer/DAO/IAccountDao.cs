17f4727dd16e0c94137dabe4ec8719ead50cf2f1
Hadas Marcus <hadasmarcus@yahoo.com>
2023-03-08 13:21:34 -0500

    conflict res

--- capstone/TenmoServer/DAO/IAccountDao.cs
+++ capstone/TenmoServer/DAO/IAccountDao.cs
@@ -6,8 +6,7 @@ namespace TenmoServer.DAO
     public interface IAccountDao
     {
         Account GetAccount(int accountId);
-        IList<Account> GetAccountsByUser(int userId);
-        decimal AccountBalance(decimal balance);
+        decimal ShowBalance(int accountId);
 
 
 
